b546ed13937065b8b672490395d44f1eff91b9cb
Casttle22 <wesleyrcardona@umes.edu.gt>
2025-10-25 00:48:36 -0600

    Arreglo laravel

---
+++ config/vite.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    // Laravel buscará aquí en producción:
+    'manifest' => base_path('public/build/.vite/manifest.json'),
+
+    // no toques esto salvo que hayas cambiado el directorio del build
+    'build_path' => 'build',
+
+    // (opcional) si usas dev server local
+    'dev_server' => [
+        'url' => env('VITE_DEV_SERVER_URL'),
+    ],
+];
